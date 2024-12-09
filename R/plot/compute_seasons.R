93c20dca6e29048b85d6c32eb228f48a3321ce78
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-09 21:30:21 +0100

    improve filtering

--- R/plot/compute_seasons.R
+++ R/plot/compute_seasons.R
@@ -2,23 +2,28 @@
 
 compute_seasons <- function(observations){
   
+  # -- get years from df
   years <- as.numeric(unique(format(observations$date, "%Y")))
   years <- c(years, max(years) + 1)
   
+  # -- compute summers
   summer <- data.frame(name = "summer",
                        start = as.Date(paste(years - 1, "12-01", sep = "-")),
                        end = as.Date(paste(years, "02-28", sep = "-")))
   
+  # -- compute winters
   winter <- data.frame(name = "winter",
                        start = as.Date(paste(years, "06-01", sep = "-")),
                        end = as.Date(paste(years, "08-31", sep = "-")))
   
+  # -- merge
   seasons <- rbind(winter, summer)
   
+  # -- fix start / end
   seasons <- seasons[order(seasons$start), ]
   seasons <- seasons[seasons$start >= min(observations$date), ]
-  seasons <- seasons[seasons$start <= Sys.Date(), ]
-  seasons[seasons$end > Sys.Date(), ]$end <- Sys.Date()
+  seasons <- seasons[seasons$start <= max(observations$date), ]
+  seasons[seasons$end > max(observations$date), ]$end <- max(observations$date)
   
   # -- return
   seasons
