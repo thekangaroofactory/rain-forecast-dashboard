9cd53fa1f7c98d940e46108e1a0d233bdc83d486
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-11 11:43:48 +0100

    update url

--- R/call_api.R
+++ R/call_api.R
@@ -3,7 +3,7 @@
 call_api <- function(resource = "observations", station = "IDCJDW2124", start, end){
   
   # -- build url
-  api_url <- paste0("http://127.0.0.1:9107", "/api/v1/", resource)
+  api_url <- paste0("http://127.0.0.1:4017", "/api/v1/", resource)
   
   # -- call api
   response <- RCurl::getURL(api_url)
