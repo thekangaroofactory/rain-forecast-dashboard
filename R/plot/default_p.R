ac22abc81eabb3980e599de2a6305fa64df3e0d1
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-12 15:38:32 +0100

    add default plot

---
+++ R/plot/default_p.R
@@ -0,0 +1,31 @@
+
+
+
+default_p <- function(message = "Failed to build the plot.", size = 6, color = "red"){
+  
+  # -- init
+  ggplot() +
+    
+    # -- display message
+    annotate("text", 
+             label = message,
+             x = 1, 
+             y = 1, 
+             size = size, 
+             color = color) +
+    
+    # -- remove all
+    theme(axis.line=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          legend.position="none",
+          panel.background=element_blank(),
+          panel.border=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.background=element_blank())
+  
+}
