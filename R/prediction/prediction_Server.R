859ef30ae02e5774490f16200d19e85eeaf56808
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-12 15:41:07 +0100

    implement header & inputs

--- R/prediction/prediction_Server.R
+++ R/prediction/prediction_Server.R
@@ -15,20 +15,31 @@ prediction_Server <- function(id, predictions) {
     MODULE <- paste0("[", id, "]")
     cat(MODULE, "Starting module server... \n")
     
+    # -- namespace
+    ns <- session$ns
     
-    # ----------------------------------------------------------------------------
-    # Predictions
     
-    output$date_slider <- renderUI(sliderInput(inputId = "date_slider",
+    # --------------------------------------------------------------------------
+    # Data selection
+    # --------------------------------------------------------------------------
+    
+    # -- input
+    output$date_slider <- renderUI(sliderInput(inputId = ns("date_slider"),
                                                label = "Date range",
                                                min = min(predictions$date),
                                                max = max(predictions$date),
                                                value = c(min(predictions$date), max(predictions$date))))
     
-    
+    # -- filter dataset
     selected_predictions <- reactive(
       predictions[predictions$date >= input$date_slider[1] & predictions$date <= input$date_slider[2], ])
     
+    
+    # --------------------------------------------------------------------------
+    # Outputs
+    # --------------------------------------------------------------------------
+    
+    # -- header values
     output$predict_nb <- renderText(nrow(selected_predictions()))
     output$predict_accurate <- renderText(sum(selected_predictions()$accurate, na.rm = TRUE))
     output$predict_accuracy <- renderText(paste0(
