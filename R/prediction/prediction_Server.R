bfee54017fdc331fc0181dd9eeeb5b7efa0a3b68
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-16 11:49:55 +0100

    add outputs

--- R/prediction/prediction_Server.R
+++ R/prediction/prediction_Server.R
@@ -18,6 +18,9 @@ prediction_Server <- function(id, predictions) {
     # -- namespace
     ns <- session$ns
     
+    # -- reactive objects
+    nb_removed <- reactiveVal(NULL)
+    
     
     # --------------------------------------------------------------------------
     # Data selection
@@ -31,19 +34,92 @@ prediction_Server <- function(id, predictions) {
                                                value = c(min(predictions$date), max(predictions$date))))
     
     # -- filter dataset
-    selected_predictions <- reactive(
-      predictions[predictions$date >= input$date_slider[1] & predictions$date <= input$date_slider[2], ])
+    selected_predictions <- reactive({
+      
+      # -- check if input is initialized
+      req(input$date_slider)
+      
+      # -- filter
+      pre <- predictions %>%
+        filter(
+          date >= input$date_slider[1],
+          date <= input$date_slider[2])
+      
+      # -- store nb removed in next step
+      nb_removed(sum(!pre$accurate %in% c(TRUE, FALSE)))
+      
+      # -- filter out NA
+      pre %>%
+        filter(
+          accurate %in% c(TRUE, FALSE))
+      
+    })
+    
+    # -- confusion matrix
+    c_matrix <- reactive(confusion_matrix(selected_predictions()))
+
+    
+    # **************************************************************************
+    # **************************************************************************
+    predictions_df <<- predictions
+    # **************************************************************************
+    # **************************************************************************
     
     
     # --------------------------------------------------------------------------
-    # Outputs
+    # Header section
     # --------------------------------------------------------------------------
+    
+    output$predict_model <- renderText("M1")
+    
+    output$predict_removed <- renderText(paste(nb_removed(), "predictions filtered"))
     
     # -- header values
     output$predict_nb <- renderText(nrow(selected_predictions()))
-    output$predict_accurate <- renderText(sum(selected_predictions()$accurate, na.rm = TRUE))
+    
+    # -- accurate / not accurate
+    output$predict_accurate <- renderText(sum(selected_predictions()$accurate))
+    output$predict_inaccurate <- renderText(
+      paste(sum(!selected_predictions()$accurate), "predictions KO"))
+    
+    # -- accuracy %
     output$predict_accuracy <- renderText(paste0(
-      round(sum(selected_predictions()$accurate, na.rm = TRUE) / nrow(selected_predictions()) * 100, digits = 2), "%"))
+      round(sum(selected_predictions()$accurate) / nrow(selected_predictions()) * 100, digits = 2), "%"))
+    output$predict_inaccuracy <- renderText(paste0(
+      round(sum(!selected_predictions()$accurate) / nrow(selected_predictions()) * 100, digits = 2), "% predictions KO"))
+    
+    
+    # --------------------------------------------------------------------------
+    # Confusion matrix section
+    # --------------------------------------------------------------------------
+    
+    # -- confusion matrix plot
+    output$confusion_plot <- renderPlot(p_confusion_matrix(c_matrix()), bg = "transparent")
+    
+    
+    # --------------------------------------------------------------------------
+    # Accuracy over time section
+    # --------------------------------------------------------------------------
+    
+    # -- accuracy evolution
+    output$accuracy_plot <- renderPlot(p_accuracy_over_time(selected_predictions()), bg = "transparent")
+    
+    
+    # --------------------------------------------------------------------------
+    # Precision / recall section
+    # --------------------------------------------------------------------------
+    
+    # -- precision / recall
+    precision <- reactive(c_matrix()$true_positive / (c_matrix()$true_positive + c_matrix()$false_negative))
+    recall <- reactive(c_matrix()$true_positive / (c_matrix()$true_positive + c_matrix()$false_positive))
+    
+    # -- F1 score
+    f1_score <- reactive(2 * (recall() * precision()) / (recall() + precision()))
+    
+    # -- outputs
+    output$precision <- renderText(round(precision(), digits = 2))
+    output$recall <- renderText(round(recall(), digits = 2))
+    output$f1_score <- renderText(round(f1_score(), digits = 2))
     
     
   })
