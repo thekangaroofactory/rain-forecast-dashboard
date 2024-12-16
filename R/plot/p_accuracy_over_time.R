14b49d079a4462a4c0ad4b2d2f9d8279f4a034ac
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-16 11:44:59 +0100

    first commit

---
+++ R/plot/p_accuracy_over_time.R
@@ -0,0 +1,43 @@
+
+p_accuracy_over_time <- function(predictions){
+  
+  # -- init
+  ggplot(predictions,
+         aes(
+           x = date)) +
+    
+    geom_line(
+      aes(
+        y = cum_accuracy),
+      color = "orange") +
+    
+    geom_hline(
+      aes(
+        yintercept = max(cumsum, na.rm = T) / max(count, na.rm = T)),
+      linetype = "dotted",
+      color = "grey75") +
+    
+    geom_segment(
+      data = predictions[predictions$accurate %in% c(TRUE, FALSE), ],
+      aes(
+        yend = ifelse(accurate, 0.1, -0.1) - 0.25,
+        color = ifelse(accurate, "orange", "cyan")),
+      y = -0.25,
+      linewidth = 0.1) +
+    
+    scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8),
+                       labels = c("0", "20%", "40%", "60%", "80%")) +
+    
+    # -- theme
+    theme(
+      legend.position = "none",
+      plot.background = element_blank(),
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      axis.title = element_blank(),
+      axis.text = element_text(color = "grey75"),
+      axis.ticks = element_line(color = "grey75"))
+  
+}
+
+#print(p_accuracy_over_time(predictions_df))
---
+++ R/plot/p_confusion_matrix.R
@@ -0,0 +1,68 @@
+
+
+p_confusion_matrix <- function(c_mat){
+  
+  # -- nb predictions
+  nb_pre <- sum(c_mat)
+  
+  # -- data preparation
+  df <- data.frame(
+    
+    # -- x based on normalized radiuses
+    x = c(0, c_mat$true_positive / nb_pre * 100, 100, 100 + c_mat$true_negative / nb_pre * 100),
+    
+    # -- values
+    value = c(c_mat$true_positive, 
+              c_mat$false_positive, 
+              c_mat$true_negative,
+              c_mat$false_negative),
+    
+    # -- normalize values
+    radius = c(c_mat$true_positive / nb_pre * 100, 
+               c_mat$false_positive / nb_pre * 100, 
+               c_mat$true_negative / nb_pre * 100,
+               c_mat$false_negative / nb_pre * 100),
+    
+    # -- good / bad
+    color = c(1,0,1,0))
+  
+  
+  # -- build plot & return
+  ggplot(df,
+         aes(color = color)) +
+    
+    # -- circular area
+    geom_circle(
+      aes(x0 = x,
+          y0 = 0,
+          r = radius,
+          fill = color),
+      alpha = 0.3) +
+    
+    # -- value
+    geom_text(
+      aes(x = x,
+          label = value),
+      y = 0,
+      size = 5) +
+    
+    # -- fill & color
+    scale_colour_gradient(low = "grey", high = "orange") +
+    scale_fill_gradient(low = "grey", high = "orange") +
+    
+    # -- keep it 'squared'
+    coord_fixed() +
+    
+    # -- theme
+    theme(
+      legend.position = "none",
+      plot.background = element_blank(),
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank())
+  
+}
+
+#print(p_confusion_matrix(c_matrix))
