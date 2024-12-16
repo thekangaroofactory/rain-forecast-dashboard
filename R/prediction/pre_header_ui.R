04574a1a29e2ef2ed81101ac013b799c2d2ef0a2
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-16 11:50:02 +0100

    update ui

--- R/prediction/pre_header_ui.R
+++ R/prediction/pre_header_ui.R
@@ -8,21 +8,29 @@ pre_header_ui <- function(id){
   
   # -- return
   layout_columns(
-    col_widths = c(3, 3),
+    col_widths = 3,
+    
+    value_box(
+      title = "Model",
+      theme = "bg-gradient-yellow-orange",
+      value = textOutput(ns("predict_model"))),
     
     value_box(
       title = "Predictions",
       theme = "bg-gradient-yellow-orange",
-      value = uiOutput(ns("predict_nb"))),
+      value = uiOutput(ns("predict_nb")),
+      textOutput(ns("predict_removed"))),
     
     value_box(
       title = "Accurate",
       theme = "bg-gradient-yellow-orange",
-      value = uiOutput(ns("predict_accurate"))),
+      value = uiOutput(ns("predict_accurate")),
+      textOutput(ns("predict_inaccurate"))),
     
     value_box(
       title = "Accuracy",
       theme = "bg-gradient-yellow-orange",
-      value = uiOutput(ns("predict_accuracy"))))
+      value = uiOutput(ns("predict_accuracy")),
+      textOutput(ns("predict_inaccuracy"))))
   
 }
