b340bfb254f20067789c108ddc00338258004540
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-12-20 19:08:32 +0100

    Improve message

--- R/prediction/pre_accuracy_ui.R
+++ R/prediction/pre_accuracy_ui.R
@@ -7,9 +7,15 @@ pre_accuracy_ui <- function(id){
   
   # -- return
   layout_columns(
-    col_widths = c(5, 6, 1),
+    col_widths = c(1, 5, 5, 1),
     
-    h2("Accuracy over time"),
+    p(" "),
+    
+    tagList(
+      h2("Accuracy over time"),
+      p("This chart shows how the overall accuracy evolves over time as well as the good & bad predictions (stripes)."),
+      p("Although accuracy is not a good metric by itself (it can lead to an overly-optimistic performance assessment),",
+        "it's interresting to see how it evolves over times, and select corresponding period of time to zoom on.")),
     
     plotOutput(ns("accuracy_plot")),
     
