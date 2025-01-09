776bd30fea1f9feb9d9ac7f0ead12f22b88c1f13
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-01-09 13:28:48 +0100

    update text

--- R/prediction/pre_accuracy_ui.R
+++ R/prediction/pre_accuracy_ui.R
@@ -15,7 +15,7 @@ pre_accuracy_ui <- function(id){
           h2("Accuracy over time"),
           p("This chart shows how the overall accuracy evolves over time as well as the good & bad predictions (stripes)."),
           p("Although accuracy is not a good metric by itself (it can lead to an overly-optimistic performance assessment),",
-            "it's interresting to see how it evolves over times, and select corresponding period of time to zoom on.")),
+            "it's interresting to see how it evolves over time, and select corresponding period to zoom on.")),
         
         # -- plot
         plotOutput(ns("accuracy_plot"))))
