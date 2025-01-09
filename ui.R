9e55199ed19ca8e81f0526559312df8e8a9455eb
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-01-09 14:02:12 +0100

    update about tab

--- ui.R
+++ ui.R
@@ -76,21 +76,26 @@ page_navbar(
             class = "p-5",
             
             # -- project
-            h2("About the Project"),
-            p("The original dataset is provided by the Australian - Bureau of Meteorology (BOM):", br(),
+            h2("Data source"),
+            p("The dataset used to train the prediction model is provided by the Australian - Bureau of Meteorology (BOM):", br(),
               tags$a(href = "http://www.bom.gov.au/climate/data/", target = "_blank", "http://www.bom.gov.au/climate/data/"), br(),
-              "It contains 140000+ examples, captured between 2008 and 2017 in different locations accross Australia, with daily observations"),
+              "It contains 140.000+ examples, captured between 2008 and 2017 in different locations accross Australia, with daily observations"),
+            p("The daily observations used to assess the model performance and predict rain for the next day are collected from the same data source."),
+            
+            # -- code
+            div(class = "section",
+                h2("Code"),
+                p("The code for this dashboard is available in the following GitHub repository:"),
+                tags$a(href = "https://github.com/thekangaroofactory/rain-forecast-dashboard",
+                       target = "_blank",
+                       "rain-forecast-dashboard")),
             
             # -- contact
             div(class = "section",
                 h2("Contact"),
+                p("This web application has been developped with ❤ by Philippe PERET."),
                 tags$a(href = "https://www.linkedin.com/in/philippeperet/", 
                        target = "_blank",
-                       "LinkedIn"),
-                
-                
-                tags$a(href = "https://github.com/thekangaroofactory",
-                       target = "_blank",
-                       "GitHub")))
+                       "LinkedIn")))
   
 )
